a103e2e8a313d7bd1fbdf48686146e49f93335d0
manuelramirezr <manuelramirezr@gmail.com>
2023-05-29 22:46:08 -0500

    Se suben los cambios y se ajustan funciones para mejor ejecucion tambien se agrega variables de entorno,

--- backend/uploadPdfServices.py
+++ backend/uploadPdfServices.py
@@ -3,9 +3,15 @@ from flask import Flask, request, send_file
 from PyPDF2 import PdfReader
 from deep_translator import GoogleTranslator
 from gtts import gTTS
+import os
 
+## Initialize flask app
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+## Get environment variables
+## Use export PATH_ROOT_FLASK="./backend/files/" in the terminal
+path_root = os.environ.get('PATH_ROOT_FLASK')
 
 @app.route('/uploadPdf', methods=['POST'])
 def uploadPdf():
@@ -18,9 +24,9 @@ def uploadPdf():
     ## check if the file is a pdf
     if fileExtension == 'pdf':
         ## save the file
-        file.save("files/" + fileName)
+        file.save(path_root + fileName)
         ## return success
-        readTextAndTranslate("files/" + fileName)
+        readTextAndTranslate(path_root + fileName)
         return 'File saved successfully.'
     else:
         ## return error
@@ -32,15 +38,13 @@ def readPdfAndConverToText(pdfFile):
     pdfReader = PdfReader(pdfFileObj)
     numPages = len(pdfReader.pages)
     for i in range(numPages):
-        print('Page: ', i)
         pageObj = pdfReader.pages[i]
         text = pageObj.extract_text()
-        print(text)
-        with open('text.txt', 'a', encoding='utf-8') as saveFile:
+        with open(path_root + 'text.txt', 'a', encoding='utf-8') as saveFile:
             saveFile.write(text)
-        print('Text saved successfully.')
+        print('[LOG-readPdfAndConverToText] Text saved successfully.')
     pdfFileObj.close()
-    with open('text.txt', 'r') as file:
+    with open(path_root + 'text.txt', 'r') as file:
         text = file.read().replace('\n', '')
 
     
@@ -48,36 +52,42 @@ def readPdfAndConverToText(pdfFile):
 
 def readTextAndTranslate(file):
     text = readPdfAndConverToText(file)    
-    print(text)
     ## Translate text
     try:
         translated_text = translate_text(text, 'es')
         if translated_text:
-            print("##############################################")
-            print(translated_text)
-            with open('translatedText.txt', 'w', encoding='utf-8') as saveFile:
+            with open(path_root + 'translatedText.txt', 'w', encoding='utf-8') as saveFile:
                 saveFile.write(translated_text)
-            text_to_audio(translated_text, file.split('.')[0] + '.mp3')
-            print('Translation saved successfully.')
+            outputFile = file.split('pdf')[0]
+            text_to_audio(translated_text,  outputFile + 'mp3')
+            print('[LOG-readTextAndTranslate] Translation saved successfully.')
         else:
-            print('Translation failed: No translation available.')
+            print('[ERROR-readTextAndTranslate] Translation failed: No translation available.')
     except Exception as e:
-        print('Translation failed:', e)
+        print('[ERROR-readTextAndTranslate] Translation failed:', e)
 
 def translate_text(text, dest_lang):
     translated_text = ''
     chunk_size = 4000  # Adjust the chunk size as per your requirement
     chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
-    
-    for chunk in chunks:
-        translated_chunk = GoogleTranslator(source='auto', target=dest_lang).translate(chunk)
-        translated_text += translated_chunk + ' '
-    
+    try: 
+        for chunk in chunks:
+            translated_chunk = GoogleTranslator(source='auto', target=dest_lang).translate(chunk)
+            translated_text += translated_chunk + ' '
+            print('[LOG-translate_text] Translation done.')
+    except Exception as e:
+        print('[ERROR-translate_text] Translation failed:', e)
     return translated_text.strip()
 
 def text_to_audio(text, output_file):
     tts = gTTS(text=text, lang='es')
-    tts.save(output_file)
+    try:
+        tts.save(output_file)
+        print('[LOG-text_to_audio] Audio saved successfully.')
+    except Exception as e:
+        print('[ERROR-text_to_audio] Audio failed:', e)
+
+    
 
 if __name__ == '__main__':
     app.run()
